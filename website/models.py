ff707e11a5c1281e533ee4d972c410eddcadc6f5
Murilo Ignacio <murilohsignacio@gmail.com>
2019-10-11 15:03:35 -0300

    Adicionar classe usuario em models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django import forms    
 
 # Create your models here.
+
+# Criando classe que irá receber todos os emails e senhas cadastrados
+class User(models.Model):
+    email = models.EmailField(max_length = 255, verbose_name = 'Email', unique = True)
+    password = models.CharField(max_length = 16, verbose_name = 'Senha')
+
+    def __str__(self):
+        return self.email
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length = 32, verbose_name = 'Nome')
+    data_nascimento = models.DateField()
+
+# class UserForm(forms.Form):
+#     birth_date= forms.DateField(label='What is your birth date?', widget=forms.SelectDateWidget)
