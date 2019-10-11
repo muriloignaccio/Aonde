810e1a1a34d19fc6abdaf008b64b3d284100ed0b
Murilo Ignacio <murilohsignacio@gmail.com>
2019-10-11 15:38:40 -0300

    Criar atributos dentro da classe Pessoa

--- website/models.py
+++ website/models.py
@@ -4,16 +4,35 @@ from django import forms
 # Create your models here.
 
 # Criando classe que irá receber todos os emails e senhas cadastrados
-class User(models.Model):
-    email = models.EmailField(max_length = 255, verbose_name = 'Email', unique = True)
-    password = models.CharField(max_length = 16, verbose_name = 'Senha')
+class Pessoa(models.Model):
+    GENEROS = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('NB', 'Não-Binário')
+    )
+    email = models.EmailField(
+        max_length = 255,
+        verbose_name = 'Email',
+        unique = True
+        )
+    password = models.CharField(
+        max_length = 16,
+        verbose_name = 'Senha'
+        )
+    nome = models.CharField(
+        max_length = 32,
+        verbose_name = 'Nome'
+    )
+    data_nascimento = models.DateField()
+    cep = models.TextField(max_length = 9)
+    genero = models.CharField(
+        verbose_name='gender',
+        choices=GENEROS
+    )
+
+
 
     def __str__(self):
         return self.email
-
-class Cadastro(models.Model):
-    nome = models.CharField(max_length = 32, verbose_name = 'Nome')
-    data_nascimento = models.DateField()
-
 # class UserForm(forms.Form):
 #     birth_date= forms.DateField(label='What is your birth date?', widget=forms.SelectDateWidget)
