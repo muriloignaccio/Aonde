96d60ef813758d5863dec75207767001862fd527
Murilo Ignacio <murilohsignacio@gmail.com>
2019-10-23 06:37:14 -0300

    Adicionar imagem na models

--- website/models.py
+++ website/models.py
@@ -43,6 +43,7 @@ class Item_perdido(models.Model):
         null = True
         )
     descricao = models.TextField()
+    imagem = models.ImageField(upload_to='static/images')
     local = models.TextField()
     pessoa = models.ForeignKey(
         Pessoa,
@@ -58,6 +59,7 @@ class Item_achado(models.Model):
         null = True
         )
     descricao = models.TextField()
+    imagem = models.ImageField(upload_to='static/images')
     local = models.TextField()
     pessoa = models.ForeignKey(
         Pessoa,
