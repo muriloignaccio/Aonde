4bfa7b01663ba99a926be2e4a2311b7a539f0330
Murilo Ignacio <murilohsignacio@gmail.com>
2019-10-23 14:23:27 -0300

    Executar mudanças na settings para o heroku

--- aonde/settings.py
+++ aonde/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '+&=o66w1gj17=9j93=b(bct4l*9gs_(294%1x@0y9a6ybi+b%o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'aonde.herokuapp.com']
 
 
 # Application definition
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'website/static')
+
+django_heroku.settings(locals())
